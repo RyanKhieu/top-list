7176546c6caf95d71a46633f1aace74bf1f2d082
Ryan Khieu <159482357+RyanKhieu@users.noreply.github.com>
2024-11-11 09:13:36 -0500

    categories

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 #include <string>
 
+std::string category[5] = {"Movies", "TV Shows", "Songs", "Youtubers", "Video Games"};
+
 int main() {
     int listSize;
+    std::cout << "What category would you like to make a list for?" << std::endl;
+    for (int i = 0; i < 5; i++) {
+        std::cout << i + 1 << ". " << category[i] << std::endl;
+    }
+    int categoryChoice;
+    std::cin >> categoryChoice;
     std::cout << "How many items should your list have?" << std::endl;
     std::cin >> listSize;
     std::cin.ignore();
@@ -11,7 +19,7 @@ int main() {
         std::cout << "Enter item " << i + 1 << ": ";
         std::getline(std::cin, list[i]);
     }
-    std::cout << "Your list is: " << std::endl;
+    std::cout << "Your list of the top " << listSize << " " << category[categoryChoice-1] << ": " << std::endl;
     for (int i = 0; i < listSize; i++) {
         std::cout << i + 1 << ". " << list[i] << std::endl;
     }
